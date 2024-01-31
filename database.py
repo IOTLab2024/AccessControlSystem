ec2b3c3bb2705fb3929619c6eb9bab87770582bc
Rekkin0 <korzenmichal5@gmail.com>
2024-02-01 00:10:02 +0100

    add index on room name

--- database.py
+++ database.py
@@ -7,57 +7,58 @@ import sqlite3
 
 DATABASE_PATH = Path(__file__).parent / 'iotDB.db'
 
+def establish_database_connection():
+    connection = sqlite3.connect(DATABASE_PATH)
+    cursor = connection.cursor()
+    return connection, cursor
+
 def delete_database():
     if DATABASE_PATH.exists():
         os.remove(DATABASE_PATH)
-        print('Old database deleted.')
-
+        print('Old database deleted')
 
 def create_database():
-    connection = sqlite3.connect(DATABASE_PATH)
-    cursor = connection.cursor()
+    connection, cursor = establish_database_connection()
     
     cursor.execute('''
         CREATE TABLE User (
-            user_id INTEGER PRIMARY KEY,
+            id INTEGER PRIMARY KEY,
             rfid TEXT UNIQUE,
-            is_authorized BOOLEAN DEFAULT false
         )
     ''')
-
     cursor.execute('''
         CREATE INDEX idx_rfid 
         ON User (rfid);
     ''')
-
     cursor.execute('''
         CREATE TABLE Room (
-            room_id INTEGER PRIMARY KEY,
+            id INTEGER PRIMARY KEY,
             name TEXT UNIQUE
         )
     ''')
-
+    cursor.execute('''
+        CREATE INDEX idx_room_name 
+        ON Room (name);
+    ''')
+    cursor.execute('''
+        CREATE TABLE AuthenticatedUserRoom (
+            user_id INTEGER,
+            room_id INTEGER,
+            PRIMARY KEY (user_id, room_id),
+            FOREIGN KEY (user_id) REFERENCES User(id),
+            FOREIGN KEY (room_id) REFERENCES Room(id)
+        )
+    ''')
     cursor.execute('''
         CREATE TABLE CurrentUserRoom (
             user_id INTEGER,
             room_id INTEGER,
             entry_timestamp DATETIME,
             PRIMARY KEY (user_id, room_id),
-            FOREIGN KEY (user_id) REFERENCES User(user_id),
-            FOREIGN KEY (room_id) REFERENCES Room(room_id)
+            FOREIGN KEY (user_id) REFERENCES User(id),
+            FOREIGN KEY (room_id) REFERENCES Room(id)
         )
     ''')
-
-    cursor.execute('''
-        CREATE TABLE AuthenticatedUserRoom (
-            user_id INTEGER,
-            room_id INTEGER,
-            PRIMARY KEY (user_id, room_id),
-            FOREIGN KEY (user_id) REFERENCES User(user_id),
-            FOREIGN KEY (room_id) REFERENCES Room(room_id)
-        )
-    ''')
-
     cursor.execute('''
         CREATE TABLE Log (
             log_id INTEGER PRIMARY KEY,
@@ -65,8 +66,8 @@ def create_database():
             room_id INTEGER,
             entry_timestamp DATETIME,
             exit_timestamp DATETIME,
-            FOREIGN KEY (user_id) REFERENCES User(user_id),
-            FOREIGN KEY (room_id) REFERENCES Room(room_id)
+            FOREIGN KEY (user_id) REFERENCES User(id),
+            FOREIGN KEY (room_id) REFERENCES Room(id)
         )
     ''')
     
